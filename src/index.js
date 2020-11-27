4ff5665de787fd0b3bd499188cf4cd1b645dcbb5
UCSCR201833 <61467767+UCSCR201833@users.noreply.github.com>
2020-11-27 15:49:30 +0530

    Rendering Elements

--- src/index.js
+++ src/index.js
@@ -4,20 +4,30 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-function formatName(user) {
-  return user.firstName + " " + user.lastName;
+// function formatName(user) {
+//   return user.firstName + " " + user.lastName;
+// }
+
+// const user = {
+//   firstName: "Mohamed",
+//   lastName: "Faaris",
+// };
+
+// const element = (
+//   <div id="root">
+//     <h1>Hello, {formatName(user)}!</h1>
+//     <h2>Good to see you here.</h2>
+//   </div>
+// );
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById("root"));
 }
 
-const user = {
-  firstName: "Mohamed",
-  lastName: "Faaris",
-};
-
-const element = (
-  <div>
-    <h1>Hello, {formatName(user)}!</h1>
-    <h2>Good to see you here.</h2>
-  </div>
-);
-
-ReactDOM.render(element, document.getElementById("root"));
+setInterval(tick, 1000);
