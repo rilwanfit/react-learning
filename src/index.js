91894f80e8c9c7630f6e395044f2f5d26120fc8e
UCSCR201833 <61467767+UCSCR201833@users.noreply.github.com>
2020-11-30 13:47:27 +0530

    Components-and-Props

--- src/index.js
+++ src/index.js
@@ -2,32 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-// function formatName(user) {
-//   return user.firstName + " " + user.lastName;
-// }
-
-// const user = {
-//   firstName: "Mohamed",
-//   lastName: "Faaris",
-// };
-
-// const element = (
-//   <div id="root">
-//     <h1>Hello, {formatName(user)}!</h1>
-//     <h2>Good to see you here.</h2>
-//   </div>
-// );
-
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById("root"));
+import reportWebVitals from './reportWebVitals';
+const rootElement = document.getElementById('root')
+function render(){
+  ReactDOM.render(<App/>, rootElement)
 }
-
-setInterval(tick, 1000);
+if(module.hot){
+  module.hot.accept('./App',function(){
+    setTimeout(render())
+  })
+}
+render()
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
